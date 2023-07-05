2249f3653936648d5c23783dbf69182a9297be87
ppauel <mail@pauel.eu>
2023-07-05 16:36:23 +0200

    Updated 404 page 🍝

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { ContentBlock, PrimaryButton } from '../components/Elements';
 import Menu from '../components/Menu';
@@ -11,7 +11,14 @@ function NotFound() {
   const toggleTheme = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light');
   };
-  
+
+  const secret = useRef();
+  const secretClassName = "secret";
+
+  const showSecret = () => {
+    secret.current.classList.remove(secretClassName);
+  }
+
   useEffect(() => {
     const themes = ['dark', 'light'];
     localStorage.setItem('theme', theme);
@@ -32,12 +39,16 @@ function NotFound() {
               <h2>Not Found</h2>
               <p>
                 The requested page was not found.
+                <span ref={secret} className='secret'>
+                  <br />
+                  <b>Shh! Text Bep <code>SP4GH3TT1</code> to receive an achievement!</b>
+                </span>
               </p>
               <div className='contentblock-actions'>
                 <PrimaryButton text="Home" destination="/" arrow />
               </div>
             </div>
-            <div className='contentblock-image'>
+            <div onClick={showSecret} style={{ cursor: "help" }} className='contentblock-image'>
               <img alt='' height="200px" src={require('../assets/illustrations/flower.svg').default} />
             </div>
           </ContentBlock>
